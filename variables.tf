e6b90c4e3c7e1080ff12613877d288c50ad430ab
Petko D. Petkov <pdp@gnucitizen.org>
2018-11-08 14:23:30 +0000

    First.

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  description = "The name for this task"
+  type        = "string"
+}
+
+variable "image" {
+  description = "The image for this task"
+  type        = "string"
+}
+
+variable "cpu" {
+  description = "The number of cpu units used by the task"
+  default     = 512
+}
+
+variable "memory" {
+  description = "The amount (in MiB) of memory used by the task"
+  default     = 1024
+}
+
+variable "log_retention_in_days" {
+  description = "Specifies the number of days you want to retain log events"
+  default     = 30
+}
